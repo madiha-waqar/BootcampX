bb9a1a70070aafd4f6af1104505d07547d1db371
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-11 22:16:21 -0400

    Add parameterized queries to teachers.js to prevent SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,7 @@
+// Importing the Pool object from the 'pg' module
 const { Pool } = require('pg');
 
+// Creating a new Pool instance with the database connection configuration
 const pool = new Pool({
   user: 'labber',
   password: '123',
@@ -7,16 +9,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// SQL query string for selecting teachers who assisted the cohort
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`;
+// Building array to store malicious code
+// $1: Pattern for LIKE query
+const values = [`%${process.argv[2]}%`];
+
+// Executing the parameterized query using the Pool
+pool.query(queryString, values)
 .then(res => {
+  // Looping through the query result rows and logging the information
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
